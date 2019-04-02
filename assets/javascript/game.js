206f8e618787d531bda3ac535ee8fbe76850089e
Alex Engelmann <alex.engelmann@gmail.com>
2019-04-01 23:31:58 -0700

    layout done starting js

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+//initialize variables
+
+var wins = 0;
+var losses = 0;
+
+//this function will be used later to generate numbers
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+
+//set the total and the crystals to random numbers
+
+
+//capture user clicks and update values accordingly
+
+
+
+//when user wins/loses, reset game
